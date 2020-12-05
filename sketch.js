ae639f09cbe9cd22b5e8464cfe2cc8ec39b9a45c
saiavighna <65997472+saiavighna@users.noreply.github.com>
2020-12-05 11:09:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var maxDrops =100;
+function preload(){
+    
+}
+
+function setup(){
+   
+    
+}
+
+function draw(){
+ for(var 1=0; i<maxDrops;i++)   {
+     maxDrops.push(new createDrop(random))
+ }
+}   
+
